969c38970f980d386571b229413c41b7f37c3038
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-09-22 03:44:02 +0200

    setup ci

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    coverageDirectory: 'test/_coverage',
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
 }
