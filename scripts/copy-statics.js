8eb71470a0e7999791a6e3209c8c7075f1c403e0
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-09-24 11:12:53 +0200

    remove devDependencies from built package.json

--- scripts/copy-statics.js
+++ scripts/copy-statics.js
@@ -15,7 +15,12 @@ function copyJsonPackage(path) {
     const jsonString = fs.readFileSync('./package.json', { encoding: 'utf8' })
     const json = JSON.parse(jsonString)
 
+    // Scripts and devDependencies don't make sense in the separate build
+    // directory: scripts won't work and devDependency are related to
+    // the root repo (the built package does not rely on jest, eslint...)
     delete json.scripts
+    delete json.devDependencies
+
     fs.writeFileSync(`${path}/package.json`, JSON.stringify(json, null, 2))
 }
 
