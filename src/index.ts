451bf5583ab682142d88229f0ffb3760757cb9e3
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-09-24 03:50:12 +0200

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
+import type {
+    CompareFunction,
+    FilterFunction,
+    MapFunction,
+    ReduceFunction,
+    TraverseCallback,
+} from './_shared'
+
+import { TraverseMethod } from './_shared'
 import { DoublyLinkedList } from './list'
 import { Queue } from './queue'
 import { Stack } from './stack'
 import { BinarySearchTree } from './tree'
+
+export type {
+    CompareFunction,
+    FilterFunction,
+    MapFunction,
+    ReduceFunction,
+    TraverseCallback,
+}
 
 export {
     DoublyLinkedList,
     Queue,
     Stack,
     BinarySearchTree,
+    TraverseMethod,
 }
