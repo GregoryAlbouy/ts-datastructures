478f6cb0e10b1311a40c3118d3ccdef9063ba712
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-09-28 16:58:54 +0200

    remove env. vars. default value

--- scripts/build.js
+++ scripts/build.js
@@ -2,10 +2,13 @@ const copyStatics = require('./copy-statics')
 const clearPackageDirectory = require('./clear-package-directory')
 const compile = require('./compile')
 
-const PATH = process.env.BUILD_PATH || './package'
-const TSCONFIG = process.env.BUILD_TSCONFIG || 'tsconfig.build.json'
+const PATH = process.env.BUILD_PATH
+const TSCONFIG = process.env.BUILD_TSCONFIG
 
 function build(path, tsconfig) {
+    if (!path) throw new Error('missing BUILD_PATH environment variable')
+    if (!tsconfig) throw new Error('missing BUILD_TSCONFIG environment variable')
+
     clearPackageDirectory(path)
     copyStatics(path)
     compile(path, tsconfig)
